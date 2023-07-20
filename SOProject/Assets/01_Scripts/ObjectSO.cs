e8ce220275ebab032683eb77fd1e6024f85df4b0
minchae123 <08minchae17@gmail.com>
2023-07-20 09:31:14 +0900

    SOProject

---
+++ SOProject/Assets/01_Scripts/ObjectSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ObjectSO : ScriptableObject
+{
+    public string monsterName;
+    public float monsterAtk;
+    public double monsterHP;
+    public int monsterMoveSpd;
+}
